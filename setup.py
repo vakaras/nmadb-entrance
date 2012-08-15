3bc8c2b4a18c20c1fd9f87869aafcd985e3bd1f0
Vytautas Astrauskas <vastrauskas@gmail.com>
2012-08-15 15:33:03 +0300

    Added install requires.

--- setup.py
+++ setup.py
@@ -18,6 +18,9 @@ setup(
             'distribute',
             ],
         install_requires=[              # Dependencies for the package.
+            'db_utils',
+            'pysheets',
+            'reportlab',
             ],
         scripts=[],                     # List of python script files.
         #data_files=[('/etc/init.d', ['init-script'])]
