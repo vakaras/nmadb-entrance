bbe8b9897be505d4d7d55993323b91477ed51177
Vytautas Astrauskas <vastrauskas@gmail.com>
2012-09-07 17:05:37 +0200

    Updated info.

--- src/nmadb_entrance/config.py
+++ src/nmadb_entrance/config.py
@@ -5,22 +5,23 @@ try:
     info = Info.objects.all()[0]
 except IndexError:
     import datetime
-    now = datetime.datetime.now()
     info = Info()
     info.address = (
-            u'Gedimino pr. 9, Vilnius LT-01103 '
-            u'(NVO Avilys, 3 auk\u0161tas)')
-    info.year = now.year
-    info.forms_send_deadline = now.date() + datetime.timedelta(days=30)
-    info.manager_name_dative = u'Vardeniui Pavardeniui'
-    info.manager_email = u'vardenis.pavardenis@example.com'
-    info.manager_phone = u'+37060000000'
-    info.admin_email = u'admin@example.com'
-    info.pupil_form_deadline = now.date() + datetime.timedelta(days=28)
-    info.entrance_fee = 100
-    info.firm_title = 'Some title'
-    info.firm_code = 'Some code'
-    info.bank_account = 'Some bank account number'
-    info.success_notification_deadline = (
-            now.date() + datetime.timedelta(days=40))
+            u'Student\u0173 g. 63a-206, Kaunas LT-51369')
+    info.address_multiline = (
+            u'Student\u0173 g. 63a-206,\nKaunas LT-51369')
+    info.year = 2012
+    info.forms_send_deadline = datetime.date(2012, 9, 20)
+    info.manager_name_dative = u'Rūtai'
+    info.manager_email = u'info@nmakademija.lt'
+    info.manager_phone = u'+37067768899'
+    info.admin_email = u'atranka@nmakademija.lt'
+    info.pupil_form_deadline = datetime.date(2012, 9, 16)
+    info.entrance_fee = 30
+    info.firm_title = (
+            u'Vie\u0161oji \u012fstaiga \u201eNacionalin\u0117 '
+            u'moksleivi\u0173 akademija\u201c'
+    info.firm_code = u'\u012e.k. 300628321'
+    info.bank_account = u'A/S LT197300010104583271'
+    info.success_notification_deadline = datetime.date(2012, 10, 7)
     info.save()
